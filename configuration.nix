ee441f4b43204b3275680b1815b32f5c5ecb8f61
Emil Kosz <emssej@gmail.com>
2023-03-12 14:20:18 +0100

    Add files via upload

--- configuration.nix
+++ configuration.nix
@@ -1,139 +1,167 @@
 { config, lib, pkgs, ... }:
 
 {
-    documentation.dev.enable = true;
-    i18n.defaultLocale = "pl_PL.UTF-8";
-    imports = [ <nixpkgs/nixos/modules/installer/scan/not-detected.nix> ];
-    powerManagement.cpuFreqGovernor = "performance";
-    security.sudo.wheelNeedsPassword = false;
-    swapDevices = [ { device = "/dev/sda2"; } ];
-    system.stateVersion = "22.11";
-    time.timeZone = "Europe/Warsaw";
-    
-    boot = {
-	cleanTmpDir = true;
-	kernelModules = [ "kvm-amd" ];
-	kernelParams = [ "mitigations=off" ];
+  documentation.dev.enable = true;
+  i18n.defaultLocale = "pl_PL.UTF-8";
+  imports = [ <nixpkgs/nixos/modules/installer/scan/not-detected.nix> ];
+  location.provider = "geoclue2";
+  powerManagement.cpuFreqGovernor = "performance";
+  security.sudo.wheelNeedsPassword = false;
+  swapDevices = [ { device = "/dev/sda2"; } ];
+  time.timeZone = "Europe/Warsaw";
+  
+  boot = {
+	  cleanTmpDir = true;
+	  kernelModules = [ "kvm-amd" ];
+	  kernelParams = [ "mitigations=off" ];
 
-	initrd = {
+	  initrd = {
 	    availableKernelModules = [ "ahci" "ohci_pci" "ehci_pci" "usb_storage" "sd_mod" "sr_mod" "rtsx_pci_sdmmc" ];
 	    luks.devices."crypted".device = "/dev/sda3";
-	};
+	  };
 
-	loader.grub = {
+	  loader.grub = {
 	    device = "/dev/sda";
 	    enable = true;
 	    memtest86.enable = true;
 	    version = 2;
-	};
-    };
+	  };
+  };
 
-    console = {
-	font = "Lat2-Terminus16";
-	keyMap = "pl";
-    };
+  console = {
+	  font = "Lat2-Terminus16";
+	  keyMap = "pl";
+  };
 
-    fileSystems = {
-	"/" = {
+  fileSystems = {
+	  "/" = {
 	    device = "/dev/mapper/crypted";
 	    fsType = "ext4";
-	};
+	  };
 
-	"/boot" = {
+	  "/boot" = {
 	    device = "/dev/sda1";
 	    fsType = "ext4";
-	};
-    };
+	  };
+  };
 
-    hardware = {
-	cpu.amd.updateMicrocode = true;
+  hardware = {
+	  cpu.amd.updateMicrocode = true;
 
-	opengl = {
+	  opengl = {
 	    driSupport = true;
 	    driSupport32Bit = true;
 	    enable = true;
 	    extraPackages = with pkgs; [ vaapiVdpau libvdpau-va-gl ];
-	};
-    };
+	  };
 
-    networking = {
-	hostName = "Laptop-Emila";
-	networkmanager.enable = true;
-	useDHCP = lib.mkDefault true;
-    };
+	  pulseaudio = {
+	    enable = true;
+	    support32Bit = true;
+	  };
+  };
 
-    nix = {
-	buildCores = 2;
-	maxJobs = lib.mkDefault 3;
-    };
+  networking = {
+	  hostName = "Laptop-Emila";
+	  networkmanager.enable = true;
+	  useDHCP = lib.mkDefault true;
+  };
 
-    nixpkgs = {
-	config.allowUnfree = true;
-	hostPlatform = lib.mkDefault "x86_64-linux";
-    };
-    
-    programs = {
-	adb.enable = true;
-	nm-applet.enable = true;
-	steam.enable = true;
-	
-	bash = {
+  nix = {
+	  buildCores = 2;
+	  maxJobs = lib.mkDefault 3;
+  };
+
+  nixpkgs = {
+	  config.allowUnfree = true;
+	  hostPlatform = lib.mkDefault "x86_64-linux";
+  };
+  
+  programs = {
+	  adb.enable = true;
+	  nm-applet.enable = true;
+	  steam.enable = true;
+	  
+	  bash = {
 	    interactiveShellInit = "unset HISTFILE";
 	    loginShellInit = "unset HISTFILE";
-	};
+	  };
 
-	git = {
+	  git = {
 	    enable = true;
-	    packages = pkgs.gitMinimal;
-	};
-    };
-    
-    services = {
-	printing.enable = true;
+	    package = pkgs.gitMinimal;
+	  };
+  };
+  
+  services = {
+	  printing.enable = true;
 
-	xserver = {
+    emacs = {
+      enable = true;
+      defaultEditor = true;
+    };
+
+	  redshift = {
+	    enable = true;
+	    temperature.night = 2700;
+	  };
+
+	  xserver = {
 	    enable = true;
 	    excludePackages = with pkgs; [ xterm ];
 	    layout = "pl";
 	    libinput.enable = true;
 	    videoDrivers = [ "ati" ];
-	    windowManager.exwm.enable = true;
-	};
-    };
 
-    system = {
-	stateVersion = "22.11";
+	    windowManager.exwm = {
+		    enable = true;
+		    enableDefaultConfig = false;
+		    extraPackages = epkgs: [ epkgs.nix-mode ];
+	    };
+	  };
+  };
 
-	autoUpgrade = {
+  system = {
+	  stateVersion = "22.11";
+
+	  autoUpgrade = {
 	    dates = "daily";
 	    enable = true;
-	};
-    };
-    
-    users = {
-	mutableUsers = false;
+	  };
+  };
 
-	extraUsers."emssej" = {
+  systemd.user.services.pasystray = {
+	  description = "PulseAudio volume control applet";
+	  wantedBy = [ "graphical-session.target" ];
+	  partOf = [ "graphical-session.target" ];
+	  serviceConfig.ExecStart = "${pkgs.pasystray}/bin/pasystray";
+  };
+  
+  users = {
+	  mutableUsers = false;
+
+	  extraUsers."emssej" = {
 	    description = "Emil Kosz";
-	    extraGroups = [ "wheel" "adbusers" "cdrom" "dialout" "pulse" "video" "networkmanager" ];
+	    extraGroups = [ "wheel" "adbusers" "cdrom" "dialout" "pulse" "video" "networkmanager" "audio" ];
 	    hashedPassword = "$6$cpcwbA063Q$GkfWogFrQ1/hcmwZcAq9bawyc1Z605sR7hyqYtWPXHYumt.szSz6BXVVbjxVc59S7OPUdk1dmOPMshRklNEXf.";
 	    isNormalUser = true;
 	    uid = 1337;
 
 	    packages = with pkgs; [
-		bleachbit
-		curl
-		file
-		firefox-bin
-		steam
-		steam-run
-		tdesktop
-		teams
-		transmission-gtk
-		unrar
-		unzip
-		zip
+		    bleachbit
+		    curl
+		    file
+		    firefox-bin
+		    pasystray
+		    steam
+		    steam-run
+		    tdesktop
+		    teams
+		    transmission-gtk
+		    unrar
+		    unzip
+		    zip
 	    ];
-	};
-    };
+	  };
+  };
 }
